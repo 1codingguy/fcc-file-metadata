67008c8b51f503c4f8b2b454a3288d87ce58cab1
coding-guy <1codingguy@gmail.com>
2022-08-31 13:03:09 +0100

    install mongoose, setup db connection function

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+var cors = require('cors')
 require('dotenv').config()
+const connectDB = require('./db/connect')
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+const port = process.env.PORT || 3000
 
+const start = () => {
+  try {
+    connectDB(process.env.MONGO_URI)
+    app.listen(port, function () {
+      console.log('Your app is listening on port ' + port)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+start()
